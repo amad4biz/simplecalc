f96ad2f1ef95dda9dd7c8d60bc523ad2cb47e671
amad4biz <thierno.diallo@its.ny.gov>
2016-09-15 14:58:14 -0400

    initial commit to push to github

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = angular.module("calcApp", []);
+
+app.controller ('clcCtrl', function($scope){
+      
+          	  $scope.amount = "200";
+			  $scope.duration="3";
+			  $scope.interest="12";
+			  
+			  $scope.payment = function(){
+				  
+				  return( ($scope.interest/1200.0*$scope.amount)/(1.0-Math.pow(1.0+$scope.interest/1200.00, -1.0*$scope.duration*12.0)) ).toFixed(2);
+				  
+			  };
+ 	
+});
+
+app.controller("accontCtrl", function(){
+	
+	   account = { balance = 0};
+	
+	  
+	  
+	
+	
+});
